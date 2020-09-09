d847d0c12f98aa5fb29f41f52b2a85d304865867
Fatima Rizvi <fatimarzv23@gmail.com>
2020-09-09 15:13:14 -0500

    set up the axios stuff to call axios and such

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+// const BASE_URL = 'https://api.nasa.gov/planetary/apod'
+// const API_KEY = 'xQSx5gLJLPk9g6tY2bQKvFaCWuSBkkWLr692JA3a'
 
 function App() {
+  const [nasaData, setNasaData] = useState([])
+
+  useEffect(() => { 
+    //This is our"side effect" our random code we want to write
+    // this will happen AFTERDOM surgery
+    axios.get(`${BASE_URL}?api_key=${API_KEY}`)
+      .then(res => {
+        debugger
+        setNasaData(res.data)
+      })
+      .catch(err => {
+        debugger
+      })
+  }, [])
+
   return (
     <div className="App">
       <p>
