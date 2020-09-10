2d79756d392ddf97ed04e28435a2d46459f98b24
Fatima Rizvi <fatimarzv23@gmail.com>
2020-09-10 14:58:03 -0500

    Began using Styled-components in photo component

--- src/Photo.js
+++ src/Photo.js
@@ -1,14 +1,38 @@
 // main-photo div to hold main photo
 import React from 'react';
+import styled, { keyframes } from 'styled-components'
+
+const kf = keyframes`
+    25% {
+        transform: scale(0.8)
+    }
+    50%{
+        transform: scale(1.2)
+    }
+    100%{
+        transform: scale(1.05)
+    }
+`
+
+const StyledImg = styled.img`
+  border: 10px solid ${pr => pr.theme.borderColor};
+  &:hover{
+        animation: ${kf} 1s forwards;
+    }
+`
+const StyledPhotoSec = styled.div`
+    background-color: ${pr => pr.theme.grey};
+    color: ${pr => pr.theme.white};
+`
 
 export default function Photo(props) {
     const { photoUrl, copyRight, date } = props
 
     return (
-        <div className = 'main-photo'>
-            <img src={photoUrl} alt="NASA's Daily Photo"/>
+        <StyledPhotoSec className = 'main-photo'>
+            <StyledImg src={photoUrl} alt="NASA's Daily Photo"/>
             <h6>Copyright: {copyRight}</h6>
             <h6>Date: {date}</h6>
-        </div>
+        </StyledPhotoSec>
     )
 }
--- src/Theme.js
+++ src/Theme.js
@@ -3,8 +3,10 @@ export default {
     primaryColor: 'darkblue',
     secondaryColor: 'crimson',
     tertiaryColor: 'mediumseagreen',
+    borderColor: '#01c4fe',
     white: 'white',
     black: 'black',
+    grey: 'grey',
     paddingSmall: '8px',
     mobileBreakpoint: '(max-width: 550px)',
   }
